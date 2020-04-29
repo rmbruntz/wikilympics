fc7b270fa23308d9a20b489d01fe53b1ed51c64c
Ryan Bruntz <rmbruntz@gmail.com>
2020-04-29 15:38:54 -0700

    game kinda works now

--- components/match/match.js
+++ components/match/match.js
@@ -1,10 +1,29 @@
 import styles from './match.module.css'
 import { useState } from 'react'
+import fetch from 'node-fetch'
+import useSWR from 'swr'
+import shuffle from 'shuffle-array'
+
+
+const fetcher = (url, _) => fetch(url).then(res => res.json());
+
+console.log(styles);
+
 
 export function Match(props) {
 
     const [total, setTotal] = useState(0);
     const [correct, setCorrect] = useState(0);
+
+    const onCorrect = () => {
+        setTotal(total + 1);
+        setCorrect(correct + 1);
+    }
+
+    const onIncorrect = () => {
+        setTotal(total + 1);
+    }
+
 
     return (
         <div className={styles.matchGame}>
@@ -12,18 +31,58 @@ export function Match(props) {
                 Current score: {correct} / {total}
             </div>
 
-            <Game />
+            <Round tot={total} onCorrect={onCorrect} onIncorrect={onIncorrect} />
         </div>
     )
 }
 
-function Game(props) {
+function Round(props) {
+
+    const { data, error } = useSWR(['/api/matchquote', props.tot], fetcher)
+
+    if (!data || error) return (
+
+        <div className={styles.error}>
+        </div>
+    )
+
+    console.log(data);
     return (
-        <div className={styles.game}>
+        <div className={styles.round}>
             <div className={styles.quote}>
+                {data.quote}
             </div>
-            <div className={styles.answers}>
-            </div>
+
+            <AnswerList correct={data.correct} incorrect={data.incorrect}
+                onCorrect={props.onCorrect} onIncorrect={props.onIncorrect} />
+        </div>
+    )
+}
+
+
+function AnswerList(props) {
+
+    const answers = [];
+    answers.push(<Answer key={props.correct} text={props.correct} onClick={props.onCorrect} />)
+    for (let incorrectAnswer of props.incorrect) {
+        answers.push(<Answer key={incorrectAnswer} text={incorrectAnswer} onClick={props.onIncorrect} />);
+    }
+
+    shuffle(answers);
+
+    return (
+        <div className={styles.answers}>
+            {answers}
+        </div>
+    )
+}
+
+function Answer(props) {
+    return (
+        <div className={styles.answer}>
+            <button onClick={props.onClick}>
+                {props.text}
+            </button>
         </div>
     )
 }
--- pages/api/matchquote.js
+++ pages/api/matchquote.js
@@ -1,12 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fetch from 'node-fetch'
 
-export default (req, res) => {
+
+export default async (req, res) => {
+
+  const wikiRes = await fetch(
+    'https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnlimit=4&rnnamespace=0')
+    .then(wRes => wRes.json())
+  console.log(wikiRes.query.random);
+
+  const random = wikiRes.query.random;
+
+  const correctId = random[0].id;
+
+  const correctRes = await fetch(
+    'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=links&pageids=' + correctId)
+    .then(cRes => cRes.json())
+
+    const links = correctRes.query.pages[correctId].links;
+
+    const linkIndex = Math.floor(Math.random() * links.length);
+
+    const mysteryLink = links[linkIndex].title;
+
+  const names = random.slice(1, random.length).map((entry) => entry.title);
+
+
   res.statusCode = 200
   res.json({
-    quote: 'yee haw',
-    articles: ['Ram Ranch',
-      'McCree',
-      'The South'],
-    correct: 'The South',
+    quote: mysteryLink,
+    incorrect: names,
+    correct: random[0].title,
   })
+  console.log(new Date())
 }
